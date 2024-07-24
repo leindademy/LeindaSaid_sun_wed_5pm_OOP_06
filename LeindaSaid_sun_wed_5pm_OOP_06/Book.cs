3d11459b7432cba075b9181783f386e3bf870032
Leinda Demy <demyleinda@gmail.com>
2024-07-24 03:19:08 +0300

    Initial commit

---
+++ LeindaSaid_sun_wed_5pm_OOP_06/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeindaSaid_sun_wed_5pm_OOP_06
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate,
+        decimal Price)
+        {
+            this.ISBN = _ISBN;
+            this.Title = _Title;
+            this.Authors = _Authors;
+            this.PublicationDate = _PublicationDate;
+            this.Price = Price;
+        }
+        public override string ToString()
+        {
+            if (this != null)
+            {
+                return this.ISBN + " " + this.Title + " " + this.Authors + " " + this.PublicationDate + " " + this.Price;
+            }
+
+            return "";
+        }
+
+        public class BookFunctions
+        {
+            public static string GetAsTitle(Book book)
+            {
+                return book.Title;
+            }
+            public static decimal GetPrice(Book book)
+            {
+                return book.Price;
+            }
+            public static string GetISBN(Book book)
+            {
+                return book.ISBN;
+            }
+            public static DateTime GetPublicationDate(Book book)
+            {
+                return book.PublicationDate;
+            }
+            public static string[] GetAuthors(Book book)
+            {
+                return book.Authors;
+            }
+        }
+    }
+}
---
+++ LeindaSaid_sun_wed_5pm_OOP_06/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.Tracing;
+using static LeindaSaid_sun_wed_5pm_OOP_06.Book;
+
+namespace LeindaSaid_sun_wed_5pm_OOP_06
+
+{
+    public delegate string GetATitleDelegate(Book book);
+    public delegate decimal GetAPriceDelegate(Book book);
+    public delegate string GetISBN(Book book);
+    public delegate DateTime GetPublicationDate(Book book);
+    public delegate Array GetAuthors(Book book);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ques_1
+            //Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            //dictionary.Add(0, "Leindaaa");
+            //bool ARepeated = false;
+            //string Nonrepeatingletters = "";
+
+            //for (int i = 0; i < dictionary.Count; i++)
+            //{
+            //    for (int j = 0; j < dictionary.Values.Count; j++)
+            //    {
+            //        //Console.WriteLine(dictionary.Values.ElementAt(j));
+            //        string name = dictionary.Values.ElementAt(j);
+            //        for (int k = 0; k < name.Length; k++)
+            //        {
+            //            for (int l = k + 1; l < name.Length; l++)
+            //            {
+            //                if (name[k] == name[l])
+            //                {
+            //                    ARepeated = true;
+            //                    break;
+
+            //                }
+            //            }
+
+            //            if (ARepeated == false)
+            //            {
+            //                Nonrepeatingletters = name[k].ToString();
+            //                Console.WriteLine(Nonrepeatingletters);
+            //                //ARepeated = true;
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region Ques_2
+            GetATitleDelegate Get_Title_Delegate = BookFunctions.GetAsTitle;
+            GetAPriceDelegate Get_Price_Delegate = BookFunctions.GetPrice;
+            GetISBN Get_ISBN_Delegate = BookFunctions.GetISBN;
+            GetPublicationDate Get_PubDate_Delegate = BookFunctions.GetPublicationDate;
+            GetAuthors Get_Authors_Delegate = BookFunctions.GetAuthors;
+            
+            #endregion
+        }
+    }
+}
+
